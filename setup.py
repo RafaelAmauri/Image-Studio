93fba0575a227bf2e928447116d9e9c9d0876433
RafaelAmauri <amauri.rafael33@gmail.com>
2025-08-19 00:38:32 -0300

    Add Extension module to setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
-from setuptools import setup
+from setuptools import setup, Extension
 from Cython.Build import cythonize
 import numpy as np
 
+extensions = [
+    Extension(
+        "include.effects.dithering.floyd_steinberg",
+        ["include/effects/dithering/floyd_steinberg.pyx"],
+        include_dirs=[np.get_include()]
+    )
+]
+
 setup(
-    ext_modules=cythonize(
-        "include/effects/dithering/floyd_steinberg.pyx",
-        compiler_directives={"language_level": "3"}
-    ),
-    include_dirs=[np.get_include()],
+    ext_modules=cythonize(extensions, compiler_directives={"language_level": "3"}),
 )
